38776d2eeee261af4aedc4a59aa177c49fd212d7
idanz19-meet <idanz19@meet.mit.edu>
2017-07-24 12:23:20 +0300

    Stopping here

---
+++ MEETinturtle.py
@@ -0,0 +1,45 @@
+import turtle
+# Everything that comes after the is a
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+#  ...and end it before the next line.
+
+# ME
+turtle.penup()
+turtle.goto (-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup()
+
+turtle.goto(-200+130,100)
+turtle.pendown()
+turtle.goto(-200+130,-100)
+turtle.penup()
+turtle.goto(-200+130,100)
+turtle.pendown()
+turtle.goto(-200+200,100)
+turtle.penup()
+
+turtle.goto(-200+130,0)
+turtle.pendown()
+turtle.goto(-200+200, 0)
+turtle.penup()
+turtle.goto(-200+130,-100)
+turtle.pendown()
+turtle.goto(-200+200,-100)
+turtle.penup()
+
+# ET
+turtle.goto(-200+230,100)
+turtle.pendown()
+turtle.goto(-200+230,-100)
+turtle.penup()
+turtle.goto(-200+230,100)
+turtle.pendown()
+turtle.goto(-200+300,100)
+turtle.mainloop()
